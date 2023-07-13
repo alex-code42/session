34ae394dee62bdbc49a16bfdbe8057a3b6b9ce52
Alex <areichel1986@gmail.com>
2023-07-13 16:51:53 +0200

    making the second form

--- challengeoftheday13.07/afternoon/js-forms-2_controlled-input-uppercase/js/index.js
+++ challengeoftheday13.07/afternoon/js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,13 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+
+input.addEventListener("input", (event) =>{
+    // console.log("yes")
+    // console.log(event)
+    console.log(input.value)
+    input.value = input.value.toUpperCase()
+  
+ 
+  })
