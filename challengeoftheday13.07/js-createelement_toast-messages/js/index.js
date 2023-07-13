5262a1814bb465ad029c2fb766c6205394ece396
Alex <areichel1986@gmail.com>
2023-07-13 12:08:25 +0200

    add and delete listItems

--- challengeoftheday13.07/js-createelement_toast-messages/js/index.js
+++ challengeoftheday13.07/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,17 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const listItem = document.createElement('li')
+  // listItem.setAttribute('class', "toast-container__message");
+  // listItem.className = "toast-container__message"
+  listItem.classList.add("toast-container__message")
+  listItem.innerHTML = "I'm a toast message.";
+ toastContainer.append(listItem);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  console.log('toastContainer: ', toastContainer);
+  toastContainer.innerHTML='';
+ 
 });
