3a05a93e0c2b87fa6eace7120aa0656c4dcba397
Alex <areichel1986@gmail.com>
2023-07-18 17:04:26 +0200

    making the map_challenge

--- challengeoftheday18.07/js-array-methods_map/index.js
+++ challengeoftheday18.07/js-array-methods_map/index.js
@@ -24,10 +24,41 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+// const lowerCaseAnswers = null; // ['as often as you like.', ...]
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+const lowerCaseAnswers = cards.map((card) =>{
+  return card.answer.toLowerCase()
+})
+console.log(lowerCaseAnswers)
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+// const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
 
-export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
+const questionsAndAnswersTogether = cards.map((card) => {
+  return card.question+" " +card.answer
+})
+console.log(questionsAndAnswersTogether)
+
+// const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+
+  const questionAndAnswer = cards.map((card) =>{
+    
+    //export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
+    let one = card.answer.toLowerCase()
+    let two =card.question+" "+card.answer 
+    let three = card.question+" "+card.answer
+    return( one, two, three)
+  })
+  console.log("letzte Übung")
+  console.log(questionAndAnswer)
+
+// ## Task
+
+// In the [`index.js`](./index.js) file, there is a `cards` array with three objects.
+
+// Below this array, you will find a couple of variables (e.g. `lowerCaseAnswers`). They all are initialized with `null`.
+
+// And now it's your turn: delete the value `null` and write code such that the variables have a value corresponding to their name. Use `map` to achieve this goal.
+
+// You will find some hints as comments.
+
+// Switch to the [`index.js`](./index.js) file and map / filtersome recipes!
