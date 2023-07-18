17a7b08bbbec1e43d23535937abf4be1ce6def5e
Alex <areichel1986@gmail.com>
2023-07-18 11:14:22 +0200

    anonymous_function

---
+++ challengeoftheday18.07/js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -0,0 +1,30 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
+  onSuccess();
+}
+
+// Call handleUserLogin below!
+
+function showWelcomeMessage(){
+  console.log("Welcome Message")
+}
+
+handleUserLogin(showWelcomeMessage)
+
+ var handleUserLogin = function () {
+  showWelcomeMessage()
+}
+handleUserLogin(showWelcomeMessage)
+
+// 1. Write a function with the "function" keyword named "showWelcomeMessage" which logs the desired text. Then
+//    call the higher order function "handleUserLogin" and pass your callback function "showWelcomeMessage" as the first argument.
+// 2. Call "handleUserLogin" again but this time write an anonymous function **inside** the function call (i.e. between the round brackets). You can write an anonymous function either:
+
+//    - as an arrow function: ` () => {...}`
+//    - as a regular function: ` function() {...}`
+
+// 3. Implement the logic from the previous from "showWelcomeMessage" inside this anonymous function.
+
+// Please switch to the `./js/index.js` file to start the challenge.
