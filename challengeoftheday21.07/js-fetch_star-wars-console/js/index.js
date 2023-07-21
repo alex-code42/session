76b4cb2e6aab53d77970d87b42abb822535f8389
Alex <areichel1986@gmail.com>
2023-07-21 16:15:36 +0200

    3po

--- challengeoftheday21.07/js-fetch_star-wars-console/js/index.js
+++ challengeoftheday21.07/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,23 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+ try{
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    
+    
+    return data; 
 
-fetchData();
+
+} catch (error) {
+    // Failure (Network error, etc)
+    console.error("An Error occurred");
+  }
+}
+
+const data = await fetchData();
+    console.log("data: ", data)
+    console.log("C3P0: ", data.results[2])
+    console.log("Eye Color: ", data.results[2].eye_color)
