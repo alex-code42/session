3ada67fe72fbd6ce948f8441f6d72293dc4e581d
Alex <areichel1986@gmail.com>
2023-07-23 12:35:35 +0200

    news_challenge solved

--- challengeoftheday20.07/js-array-methods-2_news-flash/index.js
+++ challengeoftheday20.07/js-array-methods-2_news-flash/index.js
@@ -5,14 +5,21 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
+let filteredNews = news.filter((card) => {
+  
+  return card.categories.includes("self care") && card.body.length < 260;
 });
+
+filteredNews.sort((a, b) => a.body.length - b.body.length);
+
+
 
 // Part 2 - start here
 const sortedNews = filteredNews;
 
 sortedNews.forEach((news) => {
+  // sortedNews = news.body.length > 50;
+  // console.log(news.body.length)
   const cardElement = Card(news);
   container.append(cardElement);
 });
