7a7e7936f85f66cb5dc8574cc79b973fa10c2e2e
Alex <areichel1986@gmail.com>
2023-07-28 16:00:04 +0200

    sum

--- challengeoftheday28.07/react-props_sum/src/App.js
+++ challengeoftheday28.07/react-props_sum/src/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <h1><Sum valueA={5} valueB={8}/></h1>;
+}
+
+function Sum({valueA, valueB}){
+  return <h1>{valueA+valueB}</h1>
 }
