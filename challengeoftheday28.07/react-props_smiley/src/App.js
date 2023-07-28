85761ed43673c68ae35624757d444b203f08110a
Alex <areichel1986@gmail.com>
2023-07-28 16:22:03 +0200

    smiley

--- challengeoftheday28.07/react-props_smiley/src/App.js
+++ challengeoftheday28.07/react-props_smiley/src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+  <h1>
+    <br></br>
+    <Smiley ishappy={true} />
+    <br></br>
+    <Smiley ishappy={false} />
+  </h1>
+  )
+}
+
+function Smiley({ishappy}){
+  return ishappy ? "😀" : "🤢";
 }
