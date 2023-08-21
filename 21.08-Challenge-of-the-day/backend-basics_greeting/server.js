3eb943ec188e5460d93603479b4910cd164978d4
Alex <areichel1986@gmail.com>
2023-08-21 10:53:10 +0200

    backend_basics

--- 21.08-Challenge-of-the-day/backend-basics_greeting/server.js
+++ 21.08-Challenge-of-the-day/backend-basics_greeting/server.js
@@ -1,3 +1,16 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("Hello Worldd");
+  } 
+  if (request.url === "/alex") {
+    response.statusCode = 200;
+    response.end('Hello Alex');
+  } 
+  else {
+    response.statusCode = 404;
+    response.end("Not Found");
+  }
+});
